84c1465e7d0b02c8e5e143442b7a6c4f8c4e9d5e
Mike Riley <dev+mike@blinqmedia.com>
2014-10-01 14:21:12 -0400

    refactoring improving file parser

--- services/file_parser.rb
+++ services/file_parser.rb
@@ -4,10 +4,8 @@ class FileParser
   attr_reader :input
 
   def initialize(input)
-    # We have a number of text files as our inputs
+    binding.pry
     @input = input
-    @output = []
-    @type = ""
     get_the_output_from_each_file
     output_based_on_sort
   end
@@ -17,9 +15,10 @@ class FileParser
   end
 
   def get_the_output_from_each_file
+    @output = []
     @input.each do |input|
-      @type = DelimitedString.new(input).determine_type
-      @output = OutputFormatter.new(@type, input).output_initializer
+      type = DelimitedString.new(input).determine_type
+      @output = OutputFormatter.new(type, input).output_initializer
     end
     @output
   end
