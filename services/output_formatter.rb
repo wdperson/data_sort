68948d99f728488aeb0e77be41b33f6b4b5b2a37
Mike Riley <dev+mike@blinqmedia.com>
2014-10-07 23:43:26 -0400

    clean up of output display

--- services/output_formatter.rb
+++ services/output_formatter.rb
@@ -6,9 +6,9 @@ class OutputFormatter
 
   def display
     @data.each_with_index do |data,index|
-      print "Output: #{index+1}\n"
+      print "Output #{index+1}:\n"
       data.each do |hash|
-        print "#{hash["last_name"]} #{hash["first_name"]} #{hash["campus"]} #{date_format(hash["date_of_birth"])} #{hash["favorite_color"]} \n"
+        print "#{hash["last_name"]} #{hash["first_name"]} #{hash["campus"]} #{date_format(hash["date_of_birth"])} #{hash["favorite_color"]}\n"
       end
       print "\n"
     end
