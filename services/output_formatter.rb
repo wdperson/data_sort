b17c9fffaf4808416c49a49905b84af9b27b4ea3
Mike Riley <wdperson@gmail.com>
2020-03-02 21:45:34 -0500

    refactor output formatter

--- services/output_formatter.rb
+++ services/output_formatter.rb
@@ -1,14 +1,17 @@
-class OutputFormatter
+# frozen_string_literal: true
 
+# Output Formatter service to format data.
+
+class OutputFormatter
   def initialize(data)
     @data = data
   end
 
   def display
-    @data.each_with_index do |data,index|
-      print "Output #{index+1}:\n"
+    @data.each_with_index do |data, index|
+      print "Output #{index + 1}:\n"
       data.each do |hash|
-        print "#{hash["last_name"]} #{hash["first_name"]} #{hash["campus"]} #{date_format(hash["date_of_birth"])} #{hash["favorite_color"]}\n"
+        print "#{hash['last_name']} #{hash['first_name']} #{hash['campus']} #{date_format(hash['date_of_birth'])} #{hash['favorite_color']}\n"
       end
       print "\n"
     end
--- spec/services/output_formatter_spec.rb
+++ spec/services/output_formatter_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 describe OutputFormatter do
   describe '#display' do
     subject { capture_stdout { described_class.new(data).display } }
@@ -5,15 +7,23 @@ describe OutputFormatter do
     let(:data) do
       [
         [
-          {"last_name"=>"Cummerata", "first_name"=>"Elliot", "campus"=>"New York City", "favorite_color"=>"Neon Carrot", "date_of_birth"=>Date.new(1947,04,03)},
-          {"last_name"=>"Parker", "first_name"=>"Matteo", "campus"=>"Melbourne", "favorite_color"=>"Burnt Sienna", "date_of_birth"=>Date.new(1947,04,03)},
-          {"last_name"=>"Kirlin", "first_name"=>"Mckayla", "campus"=>"Atlanta", "favorite_color"=>"Maroon", "date_of_birth"=>Date.new(1947,04,03)}
+          { 'last_name' => 'Cummerata', 'first_name' => 'Elliot',
+            'campus' => 'New York City', 'favorite_color' => 'Neon Carrot',
+            'date_of_birth' => Date.new(1947, 0o4, 0o3) },
+          { 'last_name' => 'Parker', 'first_name' => 'Matteo',
+            'campus' => 'Melbourne', 'favorite_color' => 'Burnt Sienna',
+            'date_of_birth' => Date.new(1947, 0o4, 0o3) },
+          { 'last_name' => 'Kirlin', 'first_name' => 'Mckayla',
+            'campus' => 'Atlanta', 'favorite_color' => 'Maroon',
+            'date_of_birth' => Date.new(1947, 0o4, 0o3) }
         ]
       ]
     end
 
     let(:output_to_display) do
-      "Output 1:\nCummerata Elliot New York City 4/3/1947 Neon Carrot\nParker Matteo Melbourne 4/3/1947 Burnt Sienna\nKirlin Mckayla Atlanta 4/3/1947 Maroon\n\n"
+      "Output 1:\nCummerata Elliot New York City 4/3/1947 Neon Carrot\n"\
+      "Parker Matteo Melbourne 4/3/1947 Burnt Sienna\nKirlin Mckayla "\
+      "Atlanta 4/3/1947 Maroon\n\n"
     end
 
     it { is_expected.to eq output_to_display }
