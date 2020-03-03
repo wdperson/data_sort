b69c42b0bc20ce1838eac96435d8d33c2711c5b8
Mike Riley <wdperson@gmail.com>
2020-03-02 21:44:47 -0500

    refactor sorter

--- services/sorter.rb
+++ services/sorter.rb
@@ -1,21 +1,37 @@
+# frozen_string_literal: true
+
 require 'date'
 
+# Sorter service to take in parsed data and then sort
+# based on requirements
 class Sorter
+  DELIMITERS = /[,$|]/.freeze
+  KEYS = {
+    '$' => %w[last_name first_name middle_initial campus date_of_birth favorite_color],
+    ',' => %w[last_name first_name campus favorite_color date_of_birth],
+    '|' => %w[last_name first_name middle_initial campus favorite_color date_of_birth]
+  }.freeze
 
-  def initialize(parsed_data)
-    @parsed_data = parsed_data
+  def initialize(input)
+    @input = input
+    @sorted_output = []
+  end
+
+  def sort
+    @sorted_output << sorted_by_campus_and_last_name
+    @sorted_output << sorted_by_date_of_birth
+    @sorted_output << sorted_by_last_name_desc
   end
 
   def sorted_by_campus_and_last_name
-    @parsed_data.sort_by {|pd| [pd["campus"], pd["last_name"]]}
+    @input.flatten.sort_by { |pd| [pd['campus'], pd['last_name']] }
   end
 
   def sorted_by_date_of_birth
-    @parsed_data.sort_by! {|pd| pd["date_of_birth"] }
+    @input.flatten.sort_by { |pd| pd['date_of_birth'] }
   end
 
   def sorted_by_last_name_desc
-    @parsed_data.sort_by {|pd| pd["last_name"] }.reverse
+    @input.flatten.sort_by { |pd| pd['last_name'] }.reverse
   end
-
 end
--- spec/services/sorter_spec.rb
+++ spec/services/sorter_spec.rb
@@ -1,72 +1,162 @@
-describe Sorter do
+# frozen_string_literal: true
 
-  describe "#sort" do
+describe Sorter do
+  describe '#sort' do
     let(:parsed_data) do
       [
-        {"last_name"=>"Kirlin", "first_name"=>"Mckayla", "campus"=>"Atlanta", "favorite_color"=>"Maroon", "date_of_birth"=>"5/29/1986"},
-        {"last_name"=>"Parker", "first_name"=>"Matteo", "campus"=>"Melbourne", "favorite_color"=>"Burnt Sienna", "date_of_birth"=>"2/14/1962"},
-        {"last_name"=>"Cummerata", "first_name"=>"Elliot", "campus"=>"New York City", "favorite_color"=>"Neon Carrot", "date_of_birth"=>"4/3/1947"},
-        {"last_name"=>"Nolan", "first_name"=>"Rhiannon", "middle_initial"=>"O", "campus"=>"Los Angeles", "date_of_birth"=>"10-4-1974", "favorite_color"=>"Vivid Tangerine"},
-        {"last_name"=>"Wilkinson", "first_name"=>"Stacy", "middle_initial"=>"D", "campus"=>"New York City", "date_of_birth"=>"1-22-1964", "favorite_color"=>"Shocking Pink"},
-        {"last_name"=>"Bruen", "first_name"=>"Rigoberto", "middle_initial"=>"F", "campus"=>"San Francisco", "date_of_birth"=>"12-1-1962", "favorite_color"=>"Raw Umber"},
-        {"last_name"=>"Goyette", "first_name"=>"Timmothy", "middle_initial"=>"E", "campus"=>"London", "favorite_color"=>"Pacific Blue", "date_of_birth"=>"10-2-1964"},
-        {"last_name"=>"Bednar", "first_name"=>"Filomena", "middle_initial"=>"B", "campus"=>"New York City", "favorite_color"=>"Salmon", "date_of_birth"=>"1-24-1980"},
-        {"last_name"=>"Barrows", "first_name"=>"Anika", "middle_initial"=>"U", "campus"=>"Hong Kong", "favorite_color"=>"Spring Green", "date_of_birth"=>"5-5-1965"}
+        { 'last_name' => 'Kirlin', 'first_name' => 'Mckayla',
+          'campus' => 'Atlanta', 'favorite_color' => 'Maroon',
+          'date_of_birth' => '5/29/1986' },
+        { 'last_name' => 'Parker', 'first_name' => 'Matteo',
+          'campus' => 'Melbourne', 'favorite_color' => 'Burnt Sienna',
+          'date_of_birth' => '2/14/1962' },
+        { 'last_name' => 'Cummerata', 'first_name' => 'Elliot',
+          'campus' => 'New York City', 'favorite_color' => 'Neon Carrot',
+          'date_of_birth' => '4/3/1947' },
+        { 'last_name' => 'Nolan', 'first_name' => 'Rhiannon',
+          'middle_initial' => 'O', 'campus' => 'Los Angeles',
+          'date_of_birth' => '10-4-1974',
+          'favorite_color' => 'Vivid Tangerine' },
+        { 'last_name' => 'Wilkinson', 'first_name' => 'Stacy',
+          'middle_initial' => 'D', 'campus' => 'New York City',
+          'date_of_birth' => '1-22-1964',
+          'favorite_color' => 'Shocking Pink' },
+        { 'last_name' => 'Bruen', 'first_name' => 'Rigoberto',
+          'middle_initial' => 'F', 'campus' => 'San Francisco',
+          'date_of_birth' => '12-1-1962', 'favorite_color' => 'Raw Umber' },
+        { 'last_name' => 'Goyette', 'first_name' => 'Timmothy',
+          'middle_initial' => 'E', 'campus' => 'London',
+          'favorite_color' => 'Pacific Blue', 'date_of_birth' => '10-2-1964' },
+        { 'last_name' => 'Bednar', 'first_name' => 'Filomena',
+          'middle_initial' => 'B', 'campus' => 'New York City',
+          'favorite_color' => 'Salmon', 'date_of_birth' => '1-24-1980' },
+        { 'last_name' => 'Barrows', 'first_name' => 'Anika',
+          'middle_initial' => 'U', 'campus' => 'Hong Kong',
+          'favorite_color' => 'Spring Green', 'date_of_birth' => '5-5-1965' }
       ]
     end
 
-    context "by campus and then last name" do
-      subject { described_class.new(parsed_data).sorted_by_campus_and_last_name }
+    context 'when campus and then last name' do
+      subject do
+        described_class
+          .new(parsed_data)
+          .sorted_by_campus_and_last_name
+      end
+
       let(:sorted_campus_last_name_data) do
         [
-          {"last_name"=>"Kirlin", "first_name"=>"Mckayla", "campus"=>"Atlanta", "favorite_color"=>"Maroon", "date_of_birth"=>"5/29/1986"},
-          {"last_name"=>"Barrows", "first_name"=>"Anika", "middle_initial"=>"U", "campus"=>"Hong Kong", "favorite_color"=>"Spring Green", "date_of_birth"=>"5-5-1965"},
-          {"last_name"=>"Goyette", "first_name"=>"Timmothy", "middle_initial"=>"E", "campus"=>"London", "favorite_color"=>"Pacific Blue", "date_of_birth"=>"10-2-1964"},
-          {"last_name"=>"Nolan", "first_name"=>"Rhiannon", "middle_initial"=>"O", "campus"=>"Los Angeles", "date_of_birth"=>"10-4-1974", "favorite_color"=>"Vivid Tangerine"},
-          {"last_name"=>"Parker", "first_name"=>"Matteo", "campus"=>"Melbourne", "favorite_color"=>"Burnt Sienna", "date_of_birth"=>"2/14/1962"},
-          {"last_name"=>"Bednar", "first_name"=>"Filomena", "middle_initial"=>"B", "campus"=>"New York City", "favorite_color"=>"Salmon", "date_of_birth"=>"1-24-1980"},
-          {"last_name"=>"Cummerata", "first_name"=>"Elliot", "campus"=>"New York City", "favorite_color"=>"Neon Carrot", "date_of_birth"=>"4/3/1947"},
-          {"last_name"=>"Wilkinson", "first_name"=>"Stacy", "middle_initial"=>"D", "campus"=>"New York City", "date_of_birth"=>"1-22-1964", "favorite_color"=>"Shocking Pink"},
-          {"last_name"=>"Bruen", "first_name"=>"Rigoberto", "middle_initial"=>"F", "campus"=>"San Francisco", "date_of_birth"=>"12-1-1962", "favorite_color"=>"Raw Umber"}
+          { 'last_name' => 'Kirlin', 'first_name' => 'Mckayla',
+            'campus' => 'Atlanta', 'favorite_color' => 'Maroon',
+            'date_of_birth' => '5/29/1986' },
+          { 'last_name' => 'Barrows', 'first_name' => 'Anika',
+            'middle_initial' => 'U', 'campus' => 'Hong Kong',
+            'favorite_color' => 'Spring Green', 'date_of_birth' => '5-5-1965' },
+          { 'last_name' => 'Goyette', 'first_name' => 'Timmothy',
+            'middle_initial' => 'E', 'campus' => 'London',
+            'favorite_color' => 'Pacific Blue',
+            'date_of_birth' => '10-2-1964' },
+          { 'last_name' => 'Nolan', 'first_name' => 'Rhiannon',
+            'middle_initial' => 'O', 'campus' => 'Los Angeles',
+            'date_of_birth' => '10-4-1974',
+            'favorite_color' => 'Vivid Tangerine' },
+          { 'last_name' => 'Parker', 'first_name' => 'Matteo',
+            'campus' => 'Melbourne', 'favorite_color' => 'Burnt Sienna',
+            'date_of_birth' => '2/14/1962' },
+          { 'last_name' => 'Bednar', 'first_name' => 'Filomena',
+            'middle_initial' => 'B', 'campus' => 'New York City',
+            'favorite_color' => 'Salmon', 'date_of_birth' => '1-24-1980' },
+          { 'last_name' => 'Cummerata', 'first_name' => 'Elliot',
+            'campus' => 'New York City', 'favorite_color' => 'Neon Carrot',
+            'date_of_birth' => '4/3/1947' },
+          { 'last_name' => 'Wilkinson', 'first_name' => 'Stacy',
+            'middle_initial' => 'D', 'campus' => 'New York City',
+            'date_of_birth' => '1-22-1964',
+            'favorite_color' => 'Shocking Pink' },
+          { 'last_name' => 'Bruen', 'first_name' => 'Rigoberto',
+            'middle_initial' => 'F', 'campus' => 'San Francisco',
+            'date_of_birth' => '12-1-1962', 'favorite_color' => 'Raw Umber' }
         ]
       end
 
       it { is_expected.to eq sorted_campus_last_name_data }
     end
 
-    context 'by date of birth' do
+    context 'when date of birth' do
       subject { described_class.new(parsed_data).sorted_by_date_of_birth }
 
       let(:date_of_birth_parsed_data) do
         [
-          {"last_name"=>"Wilkinson", "first_name"=>"Stacy", "middle_initial"=>"D", "campus"=>"New York City", "date_of_birth"=>"1-22-1964", "favorite_color"=>"Shocking Pink"},
-          {"last_name"=>"Bednar", "first_name"=>"Filomena", "middle_initial"=>"B", "campus"=>"New York City", "favorite_color"=>"Salmon", "date_of_birth"=>"1-24-1980"},
-          {"last_name"=>"Goyette", "first_name"=>"Timmothy", "middle_initial"=>"E", "campus"=>"London", "favorite_color"=>"Pacific Blue", "date_of_birth"=>"10-2-1964"},
-          {"last_name"=>"Nolan", "first_name"=>"Rhiannon", "middle_initial"=>"O", "campus"=>"Los Angeles", "date_of_birth"=>"10-4-1974", "favorite_color"=>"Vivid Tangerine"},
-          {"last_name"=>"Bruen", "first_name"=>"Rigoberto", "middle_initial"=>"F", "campus"=>"San Francisco", "date_of_birth"=>"12-1-1962", "favorite_color"=>"Raw Umber"},
-          {"last_name"=>"Parker", "first_name"=>"Matteo", "campus"=>"Melbourne", "favorite_color"=>"Burnt Sienna", "date_of_birth"=>"2/14/1962"},
-          {"last_name"=>"Cummerata", "first_name"=>"Elliot", "campus"=>"New York City", "favorite_color"=>"Neon Carrot", "date_of_birth"=>"4/3/1947"},
-          {"last_name"=>"Barrows", "first_name"=>"Anika", "middle_initial"=>"U", "campus"=>"Hong Kong", "favorite_color"=>"Spring Green", "date_of_birth"=>"5-5-1965"},
-          {"last_name"=>"Kirlin", "first_name"=>"Mckayla", "campus"=>"Atlanta", "favorite_color"=>"Maroon", "date_of_birth"=>"5/29/1986"}
+          { 'last_name' => 'Wilkinson', 'first_name' => 'Stacy',
+            'middle_initial' => 'D', 'campus' => 'New York City',
+            'date_of_birth' => '1-22-1964',
+            'favorite_color' => 'Shocking Pink' },
+          { 'last_name' => 'Bednar', 'first_name' => 'Filomena',
+            'middle_initial' => 'B', 'campus' => 'New York City',
+            'favorite_color' => 'Salmon', 'date_of_birth' => '1-24-1980' },
+          { 'last_name' => 'Goyette', 'first_name' => 'Timmothy',
+            'middle_initial' => 'E', 'campus' => 'London',
+            'favorite_color' => 'Pacific Blue',
+            'date_of_birth' => '10-2-1964' },
+          { 'last_name' => 'Nolan', 'first_name' => 'Rhiannon',
+            'middle_initial' => 'O', 'campus' => 'Los Angeles',
+            'date_of_birth' => '10-4-1974',
+            'favorite_color' => 'Vivid Tangerine' },
+          { 'last_name' => 'Bruen', 'first_name' => 'Rigoberto',
+            'middle_initial' => 'F', 'campus' => 'San Francisco',
+            'date_of_birth' => '12-1-1962', 'favorite_color' => 'Raw Umber' },
+          { 'last_name' => 'Parker', 'first_name' => 'Matteo',
+            'campus' => 'Melbourne', 'favorite_color' => 'Burnt Sienna',
+            'date_of_birth' => '2/14/1962' },
+          { 'last_name' => 'Cummerata', 'first_name' => 'Elliot',
+            'campus' => 'New York City', 'favorite_color' => 'Neon Carrot',
+            'date_of_birth' => '4/3/1947' },
+          { 'last_name' => 'Barrows', 'first_name' => 'Anika',
+            'middle_initial' => 'U', 'campus' => 'Hong Kong',
+            'favorite_color' => 'Spring Green', 'date_of_birth' => '5-5-1965' },
+          { 'last_name' => 'Kirlin', 'first_name' => 'Mckayla',
+            'campus' => 'Atlanta', 'favorite_color' => 'Maroon',
+            'date_of_birth' => '5/29/1986' }
         ]
       end
 
       it { is_expected.to eq date_of_birth_parsed_data }
     end
 
-    context "by last name desc" do
+    context 'when last name desc' do
       subject { described_class.new(parsed_data).sorted_by_last_name_desc }
+
       let(:last_name_desc_parsed_data) do
         [
-          {"last_name"=>"Wilkinson", "first_name"=>"Stacy", "middle_initial"=>"D", "campus"=>"New York City", "date_of_birth"=>"1-22-1964", "favorite_color"=>"Shocking Pink"},
-          {"last_name"=>"Parker", "first_name"=>"Matteo", "campus"=>"Melbourne", "favorite_color"=>"Burnt Sienna", "date_of_birth"=>"2/14/1962"},
-          {"last_name"=>"Nolan", "first_name"=>"Rhiannon", "middle_initial"=>"O", "campus"=>"Los Angeles", "date_of_birth"=>"10-4-1974", "favorite_color"=>"Vivid Tangerine"},
-          {"last_name"=>"Kirlin", "first_name"=>"Mckayla", "campus"=>"Atlanta", "favorite_color"=>"Maroon", "date_of_birth"=>"5/29/1986"},
-          {"last_name"=>"Goyette", "first_name"=>"Timmothy", "middle_initial"=>"E", "campus"=>"London", "favorite_color"=>"Pacific Blue", "date_of_birth"=>"10-2-1964"},
-          {"last_name"=>"Cummerata", "first_name"=>"Elliot", "campus"=>"New York City", "favorite_color"=>"Neon Carrot", "date_of_birth"=>"4/3/1947"},
-          {"last_name"=>"Bruen", "first_name"=>"Rigoberto", "middle_initial"=>"F", "campus"=>"San Francisco", "date_of_birth"=>"12-1-1962", "favorite_color"=>"Raw Umber"},
-          {"last_name"=>"Bednar", "first_name"=>"Filomena", "middle_initial"=>"B", "campus"=>"New York City", "favorite_color"=>"Salmon", "date_of_birth"=>"1-24-1980"},
-          {"last_name"=>"Barrows", "first_name"=>"Anika", "middle_initial"=>"U", "campus"=>"Hong Kong", "favorite_color"=>"Spring Green", "date_of_birth"=>"5-5-1965"}
+          { 'last_name' => 'Wilkinson', 'first_name' => 'Stacy',
+            'middle_initial' => 'D', 'campus' => 'New York City',
+            'date_of_birth' => '1-22-1964',
+            'favorite_color' => 'Shocking Pink' },
+          { 'last_name' => 'Parker', 'first_name' => 'Matteo',
+            'campus' => 'Melbourne', 'favorite_color' => 'Burnt Sienna',
+            'date_of_birth' => '2/14/1962' },
+          { 'last_name' => 'Nolan', 'first_name' => 'Rhiannon',
+            'middle_initial' => 'O', 'campus' => 'Los Angeles',
+            'date_of_birth' => '10-4-1974',
+            'favorite_color' => 'Vivid Tangerine' },
+          { 'last_name' => 'Kirlin', 'first_name' => 'Mckayla',
+            'campus' => 'Atlanta', 'favorite_color' => 'Maroon',
+            'date_of_birth' => '5/29/1986' },
+          { 'last_name' => 'Goyette', 'first_name' => 'Timmothy',
+            'middle_initial' => 'E', 'campus' => 'London',
+            'favorite_color' => 'Pacific Blue',
+            'date_of_birth' => '10-2-1964' },
+          { 'last_name' => 'Cummerata', 'first_name' => 'Elliot',
+            'campus' => 'New York City', 'favorite_color' => 'Neon Carrot',
+            'date_of_birth' => '4/3/1947' },
+          { 'last_name' => 'Bruen', 'first_name' => 'Rigoberto',
+            'middle_initial' => 'F', 'campus' => 'San Francisco',
+            'date_of_birth' => '12-1-1962', 'favorite_color' => 'Raw Umber' },
+          { 'last_name' => 'Bednar', 'first_name' => 'Filomena',
+            'middle_initial' => 'B', 'campus' => 'New York City',
+            'favorite_color' => 'Salmon', 'date_of_birth' => '1-24-1980' },
+          { 'last_name' => 'Barrows', 'first_name' => 'Anika',
+            'middle_initial' => 'U', 'campus' => 'Hong Kong',
+            'favorite_color' => 'Spring Green', 'date_of_birth' => '5-5-1965' }
         ]
       end
 
